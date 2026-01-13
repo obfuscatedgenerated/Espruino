8be4287b79a4d19772a02d534c02f4896d5a0f68
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-01-13 14:23:22 +0000

    add ESP32S3_8MB board configuration

---
+++ boards/ESP32S3_8MB.py
@@ -0,0 +1,105 @@
+#!/bin/false
+# This file is part of Espruino, a JavaScript interpreter for Microcontrollers
+# Copyright (C) 2013 Gordon Williams <gw@pur3.co.uk>
+
+import pinutils;
+
+info = {
+ 'name'                     : "ESP32S3_8MB",
+ 'espruino_page_link'       : 'ESP32',
+ 'default_console'          : "EV_SERIAL1",
+ 'default_console_baudrate' : "115200",
+ 'variables'                : 65535, # Increased to force allocation into PSRAM
+ 'io_buffer_size'           : 4096,
+ 'binary_name'              : 'espruino_%v_esp32s3_8mb.bin',
+ 'build' : {
+   'optimizeflags' : '-Og',
+   'libraries' : [
+     'ESP32',
+     'NET',
+     'GRAPHICS',
+     'CRYPTO','SHA256','SHA512',
+     'TLS',
+     'TELNET',
+     'NEOPIXEL',
+     'FILESYSTEM',
+     'BLUETOOTH'
+   ],
+   'makefile' : [
+     'DEFINES+=-DESP_PLATFORM -DESP32=1',
+     'DEFINES+=-DESP_STACK_SIZE=25000',
+     'DEFINES+=-DJSVAR_MALLOC', # Required for PSRAM variable allocation
+     'DEFINES+=-DUSE_FONT_6X8',
+     'DEFINES+=-DUSB_CDC',
+     'ESP32_FLASH_MAX=1572864',
+     # --- PSRAM FLAGS (OCTAL 8MB) ---
+     'DEFINES+=-DCONFIG_SPIRAM_SUPPORT=1',
+     'DEFINES+=-DCONFIG_SPIRAM_MODE_OCT=1',
+     'DEFINES+=-DCONFIG_SPIRAM_TYPE_AUTO=1',
+     'DEFINES+=-DCONFIG_SPIRAM_SPEED_80M=1',
+     'DEFINES+=-DCONFIG_ESP32S3_SPIRAM_SUPPORT=1'
+   ]
+ }
+};
+
+chip = {
+  'part'    : "ESP32S3",
+  'family'  : "ESP32_IDF4", # S3 MUST use the IDF4 environment
+  'package' : "",
+  'ram'     : 512,
+  'flash'   : 0,
+  'speed'   : 240,
+  'usart'   : 3,
+  'spi'     : 2,
+  'i2c'     : 2,
+  'adc'     : 2,
+  'dac'     : 0,
+  'saved_code' : {
+    'address' : 0x320000,
+    'page_size' : 4096,
+    'pages' : 224, 
+    'flash_available' : 1344, 
+  },
+};
+
+devices = {
+  'LED1' : { 'pin' : 'D2' },
+  'BTN1' : { 'pin' : 'D0', "inverted":1, 'pinstate' : 'IN_PULLUP' }
+};
+
+# UI and Pin Layout (Inherited from ESP32S3)
+board_esp32 = {
+   'top' : ['GND','D23','D22','D1','D3','D21','D20','D19','D18','D5','D17','D16','D4','D0'],
+   'bottom' : ['D12','D14','D27','D26','D25','D33','D32','D35','D34','D39','D36','EN','3V3','GND'],
+   'right' : [ 'GND','D13','D9','D10','D11','D6','D7','D8','D15','D2']
+};
+board_esp32["bottom"].reverse()
+board_esp32["right"].reverse()
+board_esp32["_css"] = ""; # Standard CSS placeholder
+
+boards = [ board_esp32 ];
+
+def get_pins():
+  pins = pinutils.generate_pins(0,48)
+  
+  # I2C Definitions
+  pinutils.findpin(pins, "PD8", True)["functions"]["I2C1_SDA"]=0;
+  pinutils.findpin(pins, "PD9", True)["functions"]["I2C1_SCL"]=0;
+  pinutils.findpin(pins, "PD18", True)["functions"]["I2C2_SDA"]=0;
+  pinutils.findpin(pins, "PD19", True)["functions"]["I2C2_SCL"]=0;
+
+  # SPI Definitions
+  pinutils.findpin(pins, "PD12", True)["functions"]["SPI1_SCK"]=0;
+  pinutils.findpin(pins, "PD13", True)["functions"]["SPI1_MISO"]=0;
+  pinutils.findpin(pins, "PD11", True)["functions"]["SPI1_MOSI"]=0;
+  pinutils.findpin(pins, "PD4", True)["functions"]["SPI2_SCK"]=0;
+  pinutils.findpin(pins, "PD6", True)["functions"]["SPI2_MISO"]=0;
+  pinutils.findpin(pins, "PD7", True)["functions"]["SPI2_MOSI"]=0;
+
+  # UART Definitions
+  pinutils.findpin(pins, "PD43", True)["functions"]["USART1_TX"]=0;
+  pinutils.findpin(pins, "PD44", True)["functions"]["USART1_RX"]=0;
+  pinutils.findpin(pins, "PD17", True)["functions"]["USART2_TX"]=0;
+  pinutils.findpin(pins, "PD18", True)["functions"]["USART2_RX"]=0;
+
+  return pins
